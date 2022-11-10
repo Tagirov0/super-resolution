8e9418ade192beef92c1b51b17101177094bdc45
Tagirov0 <65162298+Tagirov0@users.noreply.github.com>
2022-11-10 22:16:03 +0300

    new

--- srgan_config.py
+++ srgan_config.py
@@ -12,4 +12,4 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 train_dataset_path = 'img_align_celeba'
 pretrained_weights = False
 weights_url = 'https://drive.google.com/file/d/15sj6S-FtNjXgevGf2EU4yKChviU2YopA/view?usp=share_link'
-pretrained_weights_path = 'checkpoint_srgan_celeba.pth.tar'
+pretrained_weights_path = 'weights/checkpoint_srgan_celeba.pth.tar'
